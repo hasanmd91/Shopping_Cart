9237d0ae5a1e7755110def440bd84ce7413f5ed1
hasanmd91 <hasanmd1991@hotmail.com>
2023-02-25 15:55:08 +0200

    build boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Pages/Home";
+import Store from "./components/Pages/Store";
+import About from "./components/Pages/About";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
+};
 
-export default App
+export default App;
