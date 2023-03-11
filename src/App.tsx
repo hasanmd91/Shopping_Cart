efa0b45ca05d5cd9e3dc084b99b9f8109cfa51d7
hasanmd91 <hasanmd1991@hotmail.com>
2023-03-12 01:31:48 +0200

    build footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,24 @@ import Store from "./Pages/Store";
 import About from "./Pages/About";
 import Navbar from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import { Footer } from "./Pages/Footer";
 
 const App = () => {
   return (
-    <ShoppingCartProvider>
-      <Navbar />
-      <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Container>
-    </ShoppingCartProvider>
+    <>
+      {" "}
+      <ShoppingCartProvider>
+        <Navbar />
+        <Container className="mb-4" style={{ minHeight: "100vh" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </ShoppingCartProvider>
+    </>
   );
 };
 
---
+++ src/Pages/Footer.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export function Footer() {
+  return (
+    <Container className="text-center text-lg-start text-muted">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="me-5 d-none d-lg-block">
+          <span>Get connected with us on social networks:</span>
+        </div>
+      </section>
+
+      <section className="">
+        <Container
+          className="text-center text-md-start p-2 "
+          style={{ backgroundColor: "black" }}
+        >
+          <Row className="mt-3">
+            <Col md="3" lg="4" xl="3" className="mx-auto mb-4 ">
+              <h6 className="text-uppercase fw-bold mb-4">Book Shop</h6>
+              <p>
+                Our company specializes in providing books written by famous
+                authors at affordable prices. We offer a wide selection of books
+                and are committed to being your go-to source for inexpensive
+                reading materials.
+              </p>
+            </Col>
+
+            <Col md="2" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+              <p>Harry Potter</p>
+              <p>Ali Baba</p>
+              <p>Masud Rana </p>
+              <p>Robinhood</p>
+            </Col>
+
+            <Col md="3" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+              <p>Pricing</p>
+              <p>Settings</p>
+              <p>Orders</p>
+              <p>Help</p>
+            </Col>
+
+            <Col md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+              <p>Helsinki Finland</p>
+              <p>info@example.com</p>
+              <p>+358 234 567 88</p>
+              <p>+358 234 567 89</p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Container>
+  );
+}
