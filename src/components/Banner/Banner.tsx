20ea6d218a21adce5c906fa1324c3c6d654e5d88
preetiag18 <preetiag18@gmail.com>
2023-03-14 20:39:59 +0200

    update banner images and fix style

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 
-import tbbanner1 from "./Images/tbanner1.jpg";
-import tbbanner2 from "./Images/tbanner2.jpg";
+import banner1 from "./Images/banner1.png";
+import banner2 from "./Images/banner2.png";
+import sale from "./Images/sale.png";
 
 const Banner = () => {
   return (
     <React.Fragment>
       <Carousel interval={2000} pause={false}>
         <Carousel.Item>
-          <img className="d-block w-100" src={tbbanner1} alt="First slide" />
+          <img className="d-block w-100" src={banner1} alt="First slide" />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={tbbanner2} alt="Second slide" />
+          <img className="d-block w-100" src={banner2} alt="Second slide" />
 
           <Carousel.Caption>
             <h3>Second slide label</h3>
@@ -24,7 +25,7 @@ const Banner = () => {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={tbbanner2} alt="Third slide" />
+          <img className="d-block w-100" src={sale} alt="Third slide" />
 
           <Carousel.Caption>
             <h3>Third slide label</h3>
