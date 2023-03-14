a0b9922e9766aed70204759f35ed428cdeeeeb49
preetiag18 <preetiag18@gmail.com>
2023-03-14 20:42:05 +0200

    remove not needed banner image texts

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -11,28 +11,12 @@ const Banner = () => {
       <Carousel interval={2000} pause={false}>
         <Carousel.Item>
           <img className="d-block w-100" src={banner1} alt="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={banner2} alt="Second slide" />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={sale} alt="Third slide" />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </React.Fragment>
